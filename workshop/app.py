7727b61f0ab51090a650d0d43f5c6b3d8275a273
Patrick S. Kanmeugne <patskan@hotmail.com>
2019-05-07 18:00:48 +0200

    fix cache issue

--- workshop/app.py
+++ workshop/app.py
@@ -1,36 +1,35 @@
 from flask import Flask, send_from_directory, request
-from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+import os
 
+root_path = ''
+app = Flask(__name__, static_url_path=root_path)
 
-class TextToSpeech(Form):
-    name = StringField('Enter the Text to translate', validators=[Required()])
-    submit = SubmitField('Submit')
-
-app = Flask(__name__, static_url_path='')
-app.config['SECRET_KEY'] = 'hard to guess string'
+def dir_last_updated(folder):
+    return str(max(os.path.getmtime(os.path.join(root_path, f))
+               for root_path, dirs, files in os.walk(folder)
+               for f in files))
 
 @app.route('/data/<path:path>')
-def send_js(path):
-    return send_from_directory('data', path)
+def servefile(path):
+    return send_from_directory('data', path, )
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     if len(request.form.get('tospeech', ''))>0 and len(request.form.get('lang', ''))>0:
         from gtts import gTTS
         tts = gTTS(text=request.form['tospeech'], lang=request.form['lang'])
-        tts.save("./data/good.mp3")
+        tts.save(os.path.join(root_path, "data/translate.mp3"))
         return '''
 <!DOCTYPE html>
 <html>
 <body>
+    <p> Saying <b>{}</b>({}) </p>
     <audio controls>
-    <source src="http://127.0.0.1:5000/data/good.mp3" type="audio/mpeg">
+    <source src="http://127.0.0.1:5000/data/translate.mp3?{}" type="audio/mpeg">
     Your browser does not support the audio element.
     </audio>
 </body>
-</html>'''
+</html>'''.format(request.form['tospeech'], request.form['lang'], dir_last_updated(os.path.join(root_path, 'data')))
     return '''
 <!DOCTYPE html>
 <html>
@@ -43,3 +42,10 @@ def hello():
     </form>
 </body>
 </html>'''
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=5000
+    )
--- workshop/voice.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-import os
-tts = gTTS(text='Bienvenu à notre Atelier', lang='fr')
-tts.save("./data/good.mp3")
-#os.system("mpg321 good.mp3")
